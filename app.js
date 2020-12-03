e9570e24349a3a504bdbf77bff9d1e2402f0cc0c
Nate Kim <enemeski@gmail.com>
2020-12-02 16:21:46 -0800

    Added pog

--- app.js
+++ app.js
@@ -22,6 +22,6 @@ app.get("/profile", function(req, res) {
 });
 
 app.listen(3000, function() {
-  console.log("Server started on port 3000");
+  console.log("Server started on port 3000 pog");
 });
 
