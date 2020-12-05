f00cef0d250977389042f5833308cc11e3fec622
Alex Warda <alexsio03@gmail.com>
2020-12-04 16:23:42 -0800

    Add login

--- app.js
+++ app.js
@@ -10,7 +10,11 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 app.get("/", function(req, res) {
-    res.render("public");
+    res.render("login");
+});
+
+app.get("/public", function(req, res) {
+  res.render("public");
 });
 
 app.get("/private", function(req, res) {
