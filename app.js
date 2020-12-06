ed0defc066f63567ddcdef040fb1a9314801e879
Nate Kim <enemeski@gmail.com>
2020-12-06 13:53:40 -0800

    Error with the .post

--- app.js
+++ app.js
@@ -13,53 +13,168 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 mongoose.connect("mongodb://localhost:27017/userinfoDB", {useNewUrlParser: true, useUnifiedTopology: true});
 
-// Login
+// Adds route for main page
 app.route("/") 
 
 // Render the main page
 .get(function(req, res) {
     res.render("login");
 }
+
 .post(function(req, res) {
   const username = req.body.uname;
   const password = req.body.pswd;
+
+  // Check if the user exists
+  User.find({name: username, password: password}, function(err, user){
+    if (err){
+      console.log(err);
+    } else {
+      if (user.length() == 0){
+        /* that user does not exist! 
+        suggest them to make an account*/
+      } else if (user.length() > 0){
+        // successful log in
+        console.log("Successfully logged in!");
+      } else {
+        // throw an error
+        console.log("There was an error logging in. Please try again.");
+      }
+    }
+  });
+
+  // Logs the given username and password
   console.log(username + " " + password);
-  res.redirect("/public");
+  // Redirects to the profile page
+  res.redirect("/profile");
 }));
 
-// Creates a new user in the database if someone registers
+// Adds the model for a user
+const userSchema = new mongoose.Schema ({
+  name: {
+    type: String,
+    minLength: 3,
+    maxLength: 20,
+    required: true
+  },
+  password: {
+    type: String,
+    minLength: 8,
+    maxLength: 30,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  }
+});
+
 const User = mongoose.model("User", userSchema);
 
+// Function to validate email
+function validateEmail(mail) 
+{
+ if (/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(mail)){
+    return (true);
+  } else {
+    return (false);
+  }
+}
+
+// Function to validate password
+function validatePassword(inputtxt) { 
+var passw =  /^[A-Za-z]\w{7,14}$/;
+
+if(inputtxt.value.match(passw)) { 
+  return true;
+  } else { 
+  return false;
+  }
+}
+
+// Registers a user if the user doesn't already exist
 app.post("/register", function(req, res) {
   const email = req.body.email;
   const username = req.body.uname;
   const password = req.body.pswd;
   const confirm = req.body.pswdConfirm;
 
+  // Checks if the given passwords were the same
   if (password != confirm){
     /* reload the register page with an error in red text at the 
     top saying that the password and confirmation don't match */
+
     console.log("The passwords did not match.");
   }
-/* check if the desired username is already taken; check if the 
-email is already being used, maybe redirect to login screen; make sure 
-the password conforms to certain rules; make sure the email has a "@" and a 
-valid ending (.com, .net, etc.) 
 
-if all of these rules are adhered to, create a new account using the given 
-information; maybe someday we will allow for profile picture selection */
+  // Check if the email is valid
+  if (!(validateEmail(email))){
+    /* return to register page, the
+    email is invalid! */
 
+    console.log("The given email was invalid!");
+  }
+
+  // Check if the password is valid
+  if (!(validatePassword(password))){
+    /* return to register page, the 
+    password is invalid! */
+
+    console.log("The given password was invalid!");
+  }
+
+  // Check if the username has been taken
+  User.find({name: username}, function(err, user){
+    if (err){
+      console.log(err);
+    } else {
+      if (user.length()  > 0){
+        /* show that username has already been taken */
+        console.log("The username has already been taken.");
+      } else if (user.length() == 0) {
+        /* the username is available; continue 
+        no console log necessary */
+      } else {
+        /* throw error */
+        console.log("There was an error entering your username.");
+      }
+    }
+  });
+
+  // Check if the email has already been used
+  User.find({email: email}, function(err, user){
+    if (err){
+      console.log(err);
+    } else {
+      if (user.length()  > 0){
+        /* show that email has already been taken 
+        maybe go to recovery page? */
+        console.log("The email has already been used.");
+      } else if (user.length() == 0) {
+        /* the email is available; continue
+        no console log necessary */
+      } else {
+        /* throw error */
+        console.log("There was an error entering your email.");
+      }
+    }
+  });
+
+  // Create a new user object with the given properties
   const user = new User ({
     name: username,
     password: password,
     email: email
   });
 
+  // Save the user to the DB
   user.save();
 
+  // Log the info to the console
   console.log(email + ", " + username + ", " + password + ", " + confirm);
 });
 
+// Render email recovery page
 app.post("/recover", function(req, res) {
   const email = req.body.email;
 
@@ -71,6 +186,7 @@ app.get("/public", function(req, res) {
   res.render("public");
 });
 
+// Filter public lobbies
 app.post("/filter", function(req, res) {
   const game = req.body.filterGame;
   const full = req.body.full;
@@ -105,7 +221,6 @@ app.get("/private", function(req, res) {
   res.render("private");
 });
 
-
 // Renders profile page
 app.get("/profile", function(req, res) {
   res.render("profile");
@@ -128,6 +243,7 @@ app.post("/gameEdit", function(req, res) {
   res.redirect("profile");
 });
 
+// Loads the page
 app.listen(3000, function() {
   console.log("Server started on port 3000");
 });
