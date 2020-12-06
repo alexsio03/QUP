a8df16520efcb70395909a013f591a70384dc1e5
Nate Kim <enemeski@gmail.com>
2020-12-06 00:27:25 -0800

    Some updates before I go to bed

--- app.js
+++ app.js
@@ -1,33 +1,61 @@
+// Requiring the main packages
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const mongoose = require('mongoose');
 
+// Setting up the app and mongoose
 const app = express();
 
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
+mongoose.connect("mongodb://localhost:27017/userinfoDB", {useNewUrlParser: true, useUnifiedTopology: true});
 
 // Login
 app.route("/") 
 
+// Render the main page
 .get(function(req, res) {
     res.render("login");
-})
-
+}
 .post(function(req, res) {
   const username = req.body.uname;
   const password = req.body.pswd;
   console.log(username + " " + password);
   res.redirect("/public");
-});
+}));
+
+// Creates a new user in the database if someone registers
+const User = mongoose.model("User", userSchema);
 
 app.post("/register", function(req, res) {
   const email = req.body.email;
   const username = req.body.uname;
   const password = req.body.pswd;
   const confirm = req.body.pswdConfirm;
+
+  if (password != confirm){
+    /* reload the register page with an error in red text at the 
+    top saying that the password and confirmation don't match */
+    console.log("The passwords did not match.");
+  }
+/* check if the desired username is already taken; check if the 
+email is already being used, maybe redirect to login screen; make sure 
+the password conforms to certain rules; make sure the email has a "@" and a 
+valid ending (.com, .net, etc.) 
+
+if all of these rules are adhered to, create a new account using the given 
+information; maybe someday we will allow for profile picture selection */
+
+  const user = new User ({
+    name: username,
+    password: password,
+    email: email
+  });
+
+  user.save();
 
   console.log(email + ", " + username + ", " + password + ", " + confirm);
 });
@@ -38,8 +66,7 @@ app.post("/recover", function(req, res) {
   console.log(email);
 });
 
-
-// Public
+// Look for public lobbies
 app.get("/public", function(req, res) {
   res.render("public");
 });
@@ -51,8 +78,7 @@ app.post("/filter", function(req, res) {
   res.redirect("/public");
 });
 
-
-// Queue Creation
+// Creates a public queue
 app.post("/create", function(req, res) {
   const game = req.body.game;
   const desc = req.body.desc;
@@ -63,6 +89,7 @@ app.post("/create", function(req, res) {
   res.redirect("/public");
 });
 
+// Creates a private queue
 app.post("/privateCreate", function(req, res) {
   const game = req.body.game;
   const desc = req.body.desc;
@@ -73,25 +100,26 @@ app.post("/privateCreate", function(req, res) {
   res.redirect("/private");
 });
 
-
-// Private
+// Renders private lobbies page
 app.get("/private", function(req, res) {
   res.render("private");
 });
 
 
-// Profile
+// Renders profile page
 app.get("/profile", function(req, res) {
   res.render("profile");
 });
 
+// Edit profile
 app.post("/userEdit", function(req, res) {
-  const newUser = req.body.newUser
-  const confirm = req.body.confirmUser;
+  const newUsername = req.body.newUsername
+  const confirm = req.body.confirmUsername;
 
   res.redirect("profile");
 });
 
+// Displays 3 favorite games on profile
 app.post("/gameEdit", function(req, res) {
   const game1 = req.body.newGame1;
   const game2 = req.body.newGame2;
@@ -101,6 +129,6 @@ app.post("/gameEdit", function(req, res) {
 });
 
 app.listen(3000, function() {
-  console.log("Server started on port 3000 pog");
+  console.log("Server started on port 3000");
 });
 
