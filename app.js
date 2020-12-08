4f10a4272dc5068c80d66ac88ed0c2941df85495
Alex Warda <alexsio03@gmail.com>
2020-12-08 13:33:45 -0800

    Add login and authentication, working

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 // Requiring the main packages
+require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
@@ -8,23 +9,9 @@ const emailValidator = require("email-validator");
 const session = require("express-session");
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
-const e = require("express");
-const LocalStrategy = require('passport-local').Strategy;
-
-passport.use(new LocalStrategy(
-  function(username, password, done) {
-    User.findOne({ username: username }, function (err, user) {
-      if (err) { return done(err); }
-      if (!user) {
-        return done(null, false, { message: 'Incorrect username.' });
-      }
-      if (!user.validPassword(password)) {
-        return done(null, false, { message: 'Incorrect password.' });
-      }
-      return done(null, user);
-    });
-  }
-));
+const {
+  json
+} = require('body-parser');
 
 // Setting up express and mongo
 const app = express();
@@ -34,24 +21,20 @@ app.use(session({
   resave: false,
   saveUninitialized: false
 }));
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(express.static("public"));
 app.use(passport.initialize());
 app.use(passport.session());
-mongoose.connect("mongodb+srv://admin-alex:superSECUREpassWORD@qup.bqake.mongodb.net/QUP?retryWrites=true&w=majority", {useNewUrlParser: true, useUnifiedTopology: true});
-
-passport.serializeUser(function(user, done) {
-  done(null, user.id);
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
 });
-
-passport.deserializeUser(function(id, done) {
-  User.findById(id, function(err, user) {
-    done(err, user);
-  });
-});
+mongoose.set('useCreateIndex', true);
 
 // Adds the model for a user
-const userSchema = new mongoose.Schema ({
+const userSchema = new mongoose.Schema({
   name: {
     type: String,
     minLength: 3,
@@ -68,28 +51,24 @@ const userSchema = new mongoose.Schema ({
     type: String,
     required: true
   },
-  friendNames: {
-    // not implemented yet
-    type: Array,
-    of: userSchema,
-    required: false
-  },
+  friends: [String],
   favoriteGames: {
     type: Array,
     of: String,
-    requried: true
-  }/*,
-  picture: {
+    required: true
+  }
+  /*,
+    picture: {
 
-  }*/
+    }*/
 });
-
-const User = mongoose.model("User", userSchema);
 
 userSchema.plugin(passportLocalMongoose);
 
+const User = new mongoose.model("User", userSchema);
+
 // Adds the model for a queue
-const queueSchema = new mongoose.Schema ({
+const queueSchema = new mongoose.Schema({
   game: {
     type: String,
     required: true
@@ -112,7 +91,7 @@ const queueSchema = new mongoose.Schema ({
   },
   lobby: {
     type: Array,
-    of: userSchema,
+    of: String,
     required: true
   },
   full: {
@@ -120,190 +99,290 @@ const queueSchema = new mongoose.Schema ({
     required: false
   }
 });
-const Queue = mongoose.model("Queue", queueSchema);
+
+const Queue = new mongoose.model("Queue", queueSchema);
 
 // Password validator scheme
 var pswdSchema = new passwordValidator();
 pswdSchema
-.is().min(8)                    // Minimum length 8
-.is().max(30)                   // Maximum length 30
-.has().uppercase()              // Must have uppercase letters
-.has().lowercase()              // Must have lowercase letters
-.has().digits(2)                // Must have at least 2 digits
-.has().not().spaces();          // Spaces not allowed
+  .is().min(8) // Minimum length 8
+  .is().max(30) // Maximum length 30
+  .has().uppercase() // Must have uppercase letters
+  .has().lowercase() // Must have lowercase letters
+  .has().digits(1) // Must have at least 2 digits
+  .has().not().spaces(); // Spaces not allowed
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(function (user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function (user, done) {
+  done(null, user);
+});
 
 // Adds route for main page and renders it
-app.route("/") 
-.get(function(req, res) {
-    res.render("login", { errorMessage: ""});
+app.route("/")
+  .get(function (req, res) {
+    res.render("login", {
+      errorMessage: ""
+    });
+  })
+
+  .post(function (req, res) {
+    const username = req.body.uname;
+    const password = req.body.pswd;
+
+    // Check if the user exists
+    User.find({
+      name: username
+    }, function (err, user) {
+      if (err) {
+        console.log(err);
+      } else {
+        if (user.length == 0) {
+          res.redirect("/error/noUser");
+        } else if (user.length > 0 && user[0].password == password) {
+          req.login(user, function (err) {
+            if (err) {
+              console.log(err);
+            } else {
+              res.redirect("/public");
+            }
+          });
+          console.log("Successfully logged in!");
+        } else {
+          // throw an error
+          res.redirect('/error/badPswd');
+          console.log("There was an error logging in. Please try again.");
+        }
+      }
+    });
+    // Logs the given username and password
+    console.log(username + " " + password);
+    // Redirects to the profile page
+  });
+
+
+
+app.get("/error/:err", function (req, res) {
+  const error = req.params.err;
+  if (error == "noUser") {
+    res.render("login", {
+      errorMessage: "No user available. Please register."
+    });
+  } else if (error == "badPswd") {
+    res.render("login", {
+      errorMessage: "Invalid password."
+    });
+  } else if (error == "userTaken") {
+    res.render("login", {
+      errorMessage: "That username was already taken."
+    });
+  } else if (error == "emailTaken") {
+    res.render("login", {
+      errorMessage: "That email was already taken."
+    });
+  } else if (error == "badRegister") {
+    res.render("login", {
+      errorMessage: "Server side error. Please report this issue."
+    });
+  } else if (error == "noMatch") {
+    res.render("login", {
+      errorMessage: "Passwords did not match."
+    });
+  } else if (error == "invalidEmail") {
+    res.render("login", {
+      errorMessage: "Email was invalid. Please use proper format."
+    });
+  } else if (error == "invalidPassword") {
+    res.render("login", {
+      errorMessage: "Password was invalid. Please use user proper format."
+    });
+  } else if (error == "login") {
+    res.render("login", {
+      errorMessage: "Please login before entering"
+    });
+  }
+
+});
+
+app.get("/logout", function (req, res) {
+  req.logout();
+  res.redirect("/");
 })
 
-.post(function(req, res) {
-  const username = req.body.uname;
-  const password = req.body.pswd;
-
-  // Check if the user exists
-  User.find({name: username}, function(err, user){
-    if (err){
-      console.log(err);
-    } else {
-      if (user.length == 0){
-        res.redirect("/error/noUser");
-      }
-      else if (user[0].password != password)
-      {
-        res.redirect("/error/badPswd");
-      } 
-      else if (user.length > 0 && user[0].password == password){
-        req.login(user, function(err) {
-          if(err)
-          {
-            console.log(err);
-          } else {
-            passport.authenticate("local", { successRedirect: '/public', failureRedirect: '/error/badPswd' });
-          }
-        });
-        res.redirect("/public");
-        console.log("Successfully logged in!");
-      }
-      else {
-        // throw an error
-        console.log("There was an error logging in. Please try again.");
-      }
-    }
-  });
-  // Logs the given username and password
-  console.log(username + " " + password);
-  // Redirects to the profile page
-});
-
-app.get("/error/:err", function(req, res) {
-  const error = req.params.err;
-  if(error == "noUser")
-  {
-    res.render("login", {errorMessage: "No user available. Please register."});
-  }
-  else if (error == "badPswd") {
-    res.render("login", {errorMessage: "Invalid password."});
-  }
-  
-});
-
 // Look for public lobbies
-app.get("/public", function(req, res) {
-  res.render("public");
+app.get("/public", function (req, res) {
+  if (req.isAuthenticated()) {
+    res.render("public");
+  } else {
+    res.redirect("/error/login");
+  }
 });
 
 // Renders private lobbies page
-app.get("/private", function(req, res) {
-  res.render("private");
+app.get("/private", function (req, res) {
+  if (req.isAuthenticated()) {
+    res.render("private");
+  } else {
+    res.redirect("/error/login");
+  }
 });
 
 // Renders profile page
-app.get("/profile", function(req, res) {
-  console.log(req.user);
+app.get("/profile", function (req, res) {
+  if (req.isAuthenticated()) {
+    res.redirect("/profile/" + req._passport.session.user[0].name);
+  } else {
+    res.redirect("/error/login");
+  }
+});
+
+app.get("/profile/:name", function (req, res) {
+  const user = req.params.name;
+
+  User.findOne({
+    name: user
+  }, function (err, found) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("profile", {
+        username: user,
+        email: found.email,
+        games: found.favoriteGames
+      });
+    }
+  })
 });
 
 // Registers a user if the user doesn't already exist
-app.post("/register", function(req, res) {
+app.post("/register", function (req, res) {
   const email = req.body.email;
   const username = req.body.uname;
   const password = req.body.pswd;
   const confirm = req.body.pswdConfirm;
   var games = ["No game selected", "No game selected", "No game selected"];
 
+
   // Checks if the given passwords were the same
-  if (password != confirm){
+  if (password != confirm) {
     /* reload the register page with an error in red text at the 
     top saying that the password and confirmation don't match */
-
+    res.redirect("/error/noMatch");
     console.log("The passwords did not match.");
   }
 
   // Check if the email is valid
-  if (!(emailValidator.validate(email))){
+  else if (!(emailValidator.validate(email))) {
     /* return to register page, the
     email is invalid! */
-
+    res.redirect("/error/invalidEmail");
     console.log("The given email was invalid!");
   }
 
   // Check if the password is valid
-  if (!(pswdSchema.validate(password))){
+  else if (!(pswdSchema.validate(password))) {
     /* return to register page, the 
     password is invalid! */
+    res.redirect("/error/invalidPassword");
+    console.log("The given password was invalid! : " + pswdSchema.validate(password, {
+      list: true
+    }));
+  } else {
+    // Check if the username has been taken
+    User.find({
+      name: username
+    }, function (err, user) {
+      if (err) {
+        console.log(err);
+      } else {
+        if (user.length > 0) {
+          res.redirect("/error/userTaken");
+        } else if (user.length == 0) {
+          User.find({
+            email: email
+          }, function (err, user) {
+            if (err) {
+              console.log(err);
+            } else {
+              if (user.length > 0) {
+                /* show that email has already been taken 
+                maybe go to recovery page? */
+                res.redirect("/error/emailTaken");
+              } else if (user.length == 0) {
+                const user = new User({
+                  name: username,
+                  password: password,
+                  email: email,
+                  favoriteGames: games
+                });
 
-    console.log("The given password was invalid! : " + pswdSchema.validate(password, { list: true }));
+                user.save();
+
+                setTimeout(function () {
+                  User.find({
+                    name: username
+                  }, function (err, user) {
+                    if (err) {
+                      console.log(err);
+                    } else {
+                      if (user.length == 0) {
+                        res.redirect("/error/noUser");
+                      } else if (user[0].password != password) {
+                        res.redirect("/error/badPswd");
+                      } else if (user.length > 0 && user[0].password == password) {
+                        req.login(user, function (err) {
+                          if (err) {
+                            console.log(err);
+                          } else {
+                            res.redirect("/public");
+                          }
+                        });
+                        console.log("Successfully logged in!");
+                      } else {
+                        // throw an error
+                        console.log("There was an error logging in. Please try again.");
+                      }
+                    }
+                  });
+                }, 3000);
+              } else {
+                /* throw error */
+                console.log("There was an error entering your email.");
+              }
+            }
+          });
+        } else {
+          /* throw error */
+          console.log("There was an error entering your username.");
+        }
+      }
+    });
   }
-
-  // Check if the username has been taken
-  User.find({name: username}, function(err, user){
-    if (err){
-      console.log(err);
-    } else {
-      if (user.length  > 0){
-        /* show that username has already been taken */
-        console.log("The username has already been taken.");
-      } else if (user.length == 0) {
-        /* the username is available; continue 
-        no console log necessary */
-      } else {
-        /* throw error */
-        console.log("There was an error entering your username.");
-      }
-    }
-  });
-
-  // Check if the email has already been used
-  User.find({email: email}, function(err, user){
-    if (err){
-      console.log(err);
-    } else {
-      if (user.length  > 0){
-        /* show that email has already been taken 
-        maybe go to recovery page? */
-        console.log("The email has already been used.");
-      } else if (user.length == 0) {
-        /* the email is available; continue
-        no console log necessary */
-      } else {
-        /* throw error */
-        console.log("There was an error entering your email.");
-      }
-    }
-  });
-
-  // Create a new user object with the given properties
-  const user = new User ({
-    name: username,
-    password: password,
-    email: email,
-    favoriteGames: games
-  });
-
-  // Save the user to the DB
-  user.save();
-
-  res.redirect("/public");
-
   // Log the info to the console
   console.log(email + ", " + username + ", " + password + ", " + confirm);
 });
 
 // Render email recovery page
-app.post("/recover", function(req, res) {
+app.post("/recover", function (req, res) {
   const email = req.body.email;
 
   console.log(email);
 });
 
 // Filter public lobbies -JS done-
-app.post("/filter", function(req, res) {
+app.post("/filter", function (req, res) {
   const game = req.body.filterGame;
   const full = req.body.full;
 
-  Queue.find({game: game, full: full}, function(err, queue){
-    if (err){
+  Queue.find({
+    game: game,
+    full: full
+  }, function (err, queue) {
+    if (err) {
       console.log(err);
     } else {
       /* only prints the filtered queues to the 
@@ -313,11 +392,20 @@ app.post("/filter", function(req, res) {
     }
   });
 
-    res.redirect("/public");
-  });
+  res.redirect("/public");
+});
 
 // Creates a public queue
-app.post("/create", function(req, res) {
+app.post("/create", function (req, res) {
+  const avai = req.body.availability;
+  if (avai == "open") {
+    res.redirect("/publicCreate");
+  } else if (avai == "friends") {
+    res.redirect("/privateCreate");
+  }
+});
+
+app.post("/publicCreate", function (req, res) {
   const game = req.body.game;
   const desc = req.body.desc;
   //REMOVE req.body.availibility FROM THE CODE!!!!!
@@ -329,28 +417,28 @@ app.post("/create", function(req, res) {
   var lobby = [];
   var isFull = true;
 
-  if (reserved.length() >= num){
+  if (reserved.length() >= num) {
     // throw error
     console.log("You cannot have more available slots than slots total.")
   }
 
-  for (var i = 0; i<reserved; i++){
+  for (var i = 0; i < reserved; i++) {
     lobby.push(reserved[i]);
   }
-  for (var i = reserved.length(); i < num; i++){
+  for (var i = reserved.length(); i < num; i++) {
     /* In the html, show that if userSchema == null,
     then show the spot as empty/available */
     lobby.push(null);
   }
 
-  for (var i = 0; i<lobby.length(); i++){
-    if (lobby[i] == null){
+  for (var i = 0; i < lobby.length(); i++) {
+    if (lobby[i] == null) {
       isFull = false;
     }
   }
 
   // Create a new queue object with the given properties
-  const queue = new Queue ({
+  const queue = new Queue({
     game: game,
     description: desc,
     visibility: visibility,
@@ -366,7 +454,7 @@ app.post("/create", function(req, res) {
 });
 
 // Creates a private queue
-app.post("/privateCreate", function(req, res) {
+app.post("/privateCreate", function (req, res) {
   const game = req.body.game;
   const desc = req.body.desc;
   //REMOVE req.body.availibility FROM THE CODE!!!!!
@@ -378,28 +466,28 @@ app.post("/privateCreate", function(req, res) {
   var lobby = [];
   var isFull = true;
 
-  if (reserved.length() >= num){
+  if (reserved.length() >= num) {
     // throw error
     console.log("You cannot have more available slots than slots total.")
   }
 
-  for (var i = 0; i<reserved; i++){
+  for (var i = 0; i < reserved; i++) {
     lobby.push(reserved[i]);
   }
-  for (var i = reserved.length(); i < num; i++){
+  for (var i = reserved.length(); i < num; i++) {
     /* In the html, show that if userSchema == null,
     then show the spot as empty/available */
     lobby.push(null);
   }
 
-  for (var i = 0; i<lobby.length(); i++){
-    if (lobby[i] == null){
+  for (var i = 0; i < lobby.length(); i++) {
+    if (lobby[i] == null) {
       isFull = false;
     }
   }
 
   // Create a new queue object with the given properties
-  const queue = new Queue ({
+  const queue = new Queue({
     game: game,
     description: desc,
     visibility: visibility,
@@ -415,13 +503,13 @@ app.post("/privateCreate", function(req, res) {
 });
 
 // Edit username -JS done-
-app.post("/userEdit", function(req, res) {
+app.post("/userEdit", function (req, res) {
   /* Enter old username
   const oldUsername = req.body.oldUsername; */
   const newUsername = req.body.newUsername
   const confirm = req.body.confirmUsername;
 
-  if (newUsername != confirm){
+  if (newUsername != confirm) {
     /* throw error and give "The usernames you entered did not match." 
     at the top of the page */
     console.log("The usernames you entered did not match.");
@@ -433,7 +521,7 @@ app.post("/userEdit", function(req, res) {
 });
 
 // Edit 3 favorite games on profile -JS done-
-app.post("/gameEdit", function(req, res) {
+app.post("/gameEdit", function (req, res) {
   /* Enter current username 
   const user = req.body.uname; */
   const game1 = req.body.newGame1;
@@ -447,7 +535,6 @@ app.post("/gameEdit", function(req, res) {
 });
 
 // Loads the page
-app.listen(3000, function() {
+app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
-
